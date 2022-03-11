5f6eeaa8a27de0587969377f46f6bc6958911674
Jacob Justice <jacob.justice@wildsparq.com>
2022-03-10 18:39:48 -0600

    Test projects

---
+++ Todos.API.Logic.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Todos.API.Logic.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
---
+++ Todos.API.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+using Xunit;
+
+namespace Todos.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
