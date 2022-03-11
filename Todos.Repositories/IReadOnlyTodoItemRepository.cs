44e1fa147595b49f5bb3ef662e3b9944367718d3
Jacob Justice <jacob.justice@wildsparq.com>
2022-03-11 00:01:46 -0600

    handler wip

---
+++ Todos.Repositories/IReadOnlyTodoItemRepository.cs
@@ -0,0 +1,10 @@
+using Todos.DTOs.Requests;
+using Todos.Models.Entities;
+using Todos.Utils.Data;
+
+namespace Todos.Repositories;
+
+public interface IReadOnlyTodoItemRepository: IReadOnlyRepository<TodoItem>
+{
+    IQueryable<TodoItem> GetAll(TodoItemQueryOptions? options);
+}
--- Todos.Repositories/TodoItemRepository.cs
+++ Todos.Repositories/TodoItemRepository.cs
@@ -1,11 +1,28 @@
 using Microsoft.EntityFrameworkCore;
+using Todos.DTOs.Requests;
 using Todos.Models;
 using Todos.Models.Entities;
 using Todos.Utils.Data;
+using Todos.Utils.Query;
 
 namespace Todos.Repositories;
 
-public class TodoItemRepository : ReadWriteRepository<TodoItem>
+public class TodoItemRepository : ReadWriteRepository<TodoItem>, IReadOnlyTodoItemRepository
 {
     public TodoItemRepository(TodoContext context) : base(context) {}
+
+    private IQueryable<TodoItem> FilterByTodoListId(IQueryable<TodoItem> query, int todoListId) =>
+        query
+            .Where(x => x.TodoListId == todoListId);
+    public IQueryable<TodoItem> GetAll(TodoItemQueryOptions? options)
+    {
+        var query = base.GetAll(options);
+        if (options.TodoListId.HasValue)
+        {
+            query = this.FilterByTodoListId(query, options.TodoListId.Value);
+        }
+
+        return query;
+    }
+    
 }
